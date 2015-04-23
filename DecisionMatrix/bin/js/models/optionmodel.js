dde694a1dcaf9446d28efc70c858ead7f19d99f4
SergioRuizRodriguez <sergio.ruiz@ooqia.com>
2015-04-23 11:48:08 -0600

    Added function calculateSumOfFeatures to class Option in optionmodel.js

---
+++ DecisionMatrix/bin/js/models/optionmodel.js
@@ -0,0 +1,6 @@
+function Option(name, features, visible)
+{
+	this.name = name;
+	this.features = features;
+	this.visible = visible;
+}
--- DecisionMatrix/js/models/optionmodel.js
+++ DecisionMatrix/js/models/optionmodel.js
@@ -3,5 +3,21 @@ function Option(name, features, visible)
 	this.name = name;
 	this.features = features;
 	this.visible = visible;
-	this.sum = 0;
+	this.sum = sum;
+
+	this.calculateSumForFeatures = function () {
+	    var sumOfFeatures = 0;
+	    if (this.features.length)
+	    {
+	        for (var feature = 0; feature < this.features.length; feature++)
+	        {
+	            if (featuresToSum[feature].value)
+	            {
+	                sumOfFeatures += featuresToSum[feature].value;
+	            }
+	        }
+	    }
+	    return sumOfFeatures;
+	};
+
 }
