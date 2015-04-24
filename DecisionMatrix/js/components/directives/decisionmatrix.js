9fccff63b80b8987bde6e26c73c8f27caf72704f
mlopezooqia <miguel.lopez@ooqia.com>
2015-04-24 09:44:57 -0700

    change decision-matriz for index

--- DecisionMatrix/js/components/directives/decisionmatrix.js
+++ DecisionMatrix/js/components/directives/decisionmatrix.js
@@ -1,6 +1,6 @@
 ﻿decisionMatrixApp.directive('horizontalDecisionMatrix', function () {
     return {
         restrict: 'A',
-        templateUrl: 'horizontal-decision-matrix.html'
+        templateUrl: 'partials/horizontal-decision-matrix.html'
     }
 });
